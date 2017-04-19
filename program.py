b23f46f9137941cbe8f9f12b97557a378b85d059
Dr3yK <wojciech.zdeb98@gmail.com>
2017-04-19 18:06:05 +0200

    Startup

--- program.py
+++ program.py
@@ -1,1 +1,6 @@
-git start master
+git clone http://gitexercises.fracz.com/git/exercises.git
+cd exercises
+git config user.name "Wojciech Zdeb"
+git config user.email "wojciech.zdeb98@gmail.com"
+./configure.sh
+git start
